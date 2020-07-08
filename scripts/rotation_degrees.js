ff88f3acb29e7d7965e3b157da7788959866814a
agreeablyblue <65247826+agreeablyblue@users.noreply.github.com>
2020-07-08 17:10:26 -0400

    rotation demo updates

--- scripts/rotation_degrees.js
+++ scripts/rotation_degrees.js
@@ -20,7 +20,7 @@ window.addEventListener('resize', function() {
   camera.updateProjectionMatrix();
 });
 
-//Line 1 creation
+//Line 1 creation (e1 at 0 y +x)
 var material = new THREE.LineBasicMaterial({color: 0x00000, linewidth: 10});
 var points = [];
 points.push(new THREE.Vector3(400, 0, 0));
@@ -31,7 +31,7 @@ var geometry = new THREE.BufferGeometry().setFromPoints(points);
 
 var line = new THREE.Line(geometry, material);
 
-//Line 2 creation
+//Line 2 creation (e2 oriented straight up)
 var material2 = new THREE.LineBasicMaterial({color: 0x000000, linewidth: 10});
 var points2 = [];
 points2.push(new THREE.Vector3(0, 0, -400));
@@ -42,30 +42,49 @@ var geometry2 = new THREE.BufferGeometry().setFromPoints(points2);
 
 var line2 = new THREE.Line(geometry2, material2);
 
+
+//Line 3 creation (e3 oriented -y -x)
+var material3 = new THREE.LineBasicMaterial({color: 0x000000, linewidth: 10});
+var points3 = [];
+points3.push(new THREE.Vector3(-200, 0, 200));
+points3.push(new THREE.Vector3(0, 0, 0));
+points3.push(new THREE.Vector3(0, 0, 0));
+
+var geometry3 = new THREE.BufferGeometry().setFromPoints(points3);
+
+var line3 = new THREE.Line(geometry3, material3);
+
+//Line 4 creation
+var material4 = new THREE.LineBasicMaterial({color: 0xff0000, linewidth: 10});
+var points4 = [];
+points4.push(new THREE.Vector3(0, 0, -400));
+points4.push(new THREE.Vector3(0, 0, 0));
+points4.push(new THREE.Vector3(0, 0, 0));
+
+var geometry4 = new THREE.BufferGeometry().setFromPoints(points4);
+
+var line4 = new THREE.Line(geometry4, material4);
+
+//Line 5 creation
+var material5 = new THREE.LineBasicMaterial({color: 0xff0000, linewidth: 10});
+var points5 = [];
+points5.push(new THREE.Vector3(400, 0, 0));
+points5.push(new THREE.Vector3(0, 0, 0));
+points5.push(new THREE.Vector3(0, 0, 0));
+
+var geometry5 = new THREE.BufferGeometry().setFromPoints(points5);
+
+var line5 = new THREE.Line(geometry5, material5);
+
+//Add lines to the scene
 scene.add(line);
 scene.add(line2);
+scene.add(line3);
+scene.add(line4);
+scene.add(line5);
 
-
-
-
-//TransformControls
-var transform = new THREE.TransformControls(camera, renderer.domElement);
-transform.setRotationSnap(THREE.Math.degToRad(0.25));
-transform.axis = 'Y';
-transform.showX = false;
-transform.showZ = false;
-
-transform.setMode("rotate");
-transform.setRotationSnap(THREE.Math.degToRad(30));
-transform.attach(line2);
-scene.add(transform);
-/*
-//Grid Helper
-var size = 850;
-var divisions = 25;
-var gridHelper = new THREE.GridHelper(size, divisions);
-scene.add(gridHelper);
-*/
+var line4Rotation = line4.rotation.y;
+var line5Rotation = line5.rotation.y;
 
 //Focuses the camera on the rendered object
 camera.position.y = 650;
@@ -73,39 +92,78 @@ camera.lookAt(0, 0, 0);
 camera.rotation.y = 90 * Math.PI / 180;
 camera.rotation.order = "YXZ";
 
-
-
-//Ambient light generator
-var pointLight = new THREE.PointLight(0xFFFFFF, 20, 1000);
-pointLight.position.set(0, 500, 0);
-scene.add(pointLight);
-
 //Function to implement orbit OrbitControls
 var orbit = new THREE.OrbitControls(camera, renderer.domElement);
 orbit.enabled = false;
 
+//Pointer to the camera movement button
+var aniButton = document.getElementById('btnStart');
+//Switch case statement variable
+var moveCase = 1;
 
-
-//Pointer to the scene reset button
-var resetButton = document.getElementById('btnReset');
-
-if(resetButton){
-  resetButton.addEventListener("click", function(){
-    location.reload();
+if (aniButton) {
+  aniButton.addEventListener("click", function() {
+      animateFirstRotation();
+      setTimeout(animateRedraw, 4000);
+      animateSecondRotation();
 
   });
 }
 
-//Function animate which calls the renderer to render the scene
-var defaultRotation = new THREE.Quaternion();
-var defaultCameraRotation = new THREE.Quaternion();
-
 var animate = function() {
   requestAnimationFrame(animate);
-
-
-
   renderer.render(scene, camera);
 };
 
+var animateFirstRotation = function(){
+  requestAnimationFrame(animateFirstRotation);
+  firstRotation();
+  renderer.render(scene, camera);
+};
+
+var animateSecondRotation = function(){
+  requestAnimationFrame(animateSecondRotation);
+  secondRotation();
+  renderer.render(scene, camera);
+};
+
+var animateRedraw = function(){
+  requestAnimationFrame(animateRedraw);
+  redrawScene();
+  renderer.render(scene, camera);
+};
+
+var redrawScene = function(){
+line4.rotation.y = line4Rotation;
+line5.rotation.y= line5Rotation;
+};
+
+var firstRotation = function( )
+{
+  if(line4.rotation.y < THREE.Math.degToRad(30))
+  {
+    line4.rotation.y += THREE.Math.degToRad(0.3);
+  }
+  if(line5.rotation.y < THREE.Math.degToRad(30))
+  {
+    line5.rotation.y += THREE.Math.degToRad(0.3);
+  }
+
+};
+
+
+
+var secondRotation = function( )
+{
+
+  if(line4.rotation.y < THREE.Math.degToRad(30))
+  {
+    line4.rotation.y += THREE.Math.degToRad(0.3);
+  }
+  if(line5.rotation.y < THREE.Math.degToRad(30))
+  {
+    line5.rotation.y += THREE.Math.degToRad(0.3);
+  }
+
+};
 animate();
